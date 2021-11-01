cb80f80149fea7d12bd9bfbbee0cfc3b70ac0631
Marcel <mrclkorbel@gmail.com>
2021-11-01 15:53:25 +0100

    Hausaufgabe1

---
+++ code/TestProjekt/sketch.js
@@ -0,0 +1,47 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(160);
+  stroke(90,30,30)
+  rect(20,280,140,20);
+  fill("brown");
+  ellipse(200,200,170,170);
+  stroke(55,80,5);
+  point(120,120);
+  line(200,200,50,40);
+  fill("purple");
+  rect(100,210,280,20);
+  fill("red");
+  stroke("10,210,0");
+  strokeWeight(5);
+  rect(180,170,70,150);
+  fill("red");
+  stroke(40,255,120);
+  strokeWeight(10);
+  rect(200,170,120,20);
+  stroke(75,20,5)
+  ellipse(100,100,20,20);
+  fill("red");
+  strokeWeight(4);
+  stroke(0,40,20)
+  line(260,60,100,10);
+  fill("yellow");
+  ellipse(200,150,50,50);
+  fill("blue");
+  stroke(0,0,180)
+  ellipse(260,80,150,25);
+  square(100,100,50);
+  stroke(3,130,0)
+  fill(20,40,0,230);
+  rect(200,100,40,60);
+  ellipse(80,300,30,40);
+  fill("yellow");
+  rect(300,30,25,160);
+  fill("white");
+  ellipse(300,320,40,40);
+  fill(20,0,85,120);
+  ellipse(280,315,35,35);
+
+}
