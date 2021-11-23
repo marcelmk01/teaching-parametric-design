c2a66343d200ad44f5b57e615ac43a3cc90af235
Marcel <mrclkorbel@gmail.com>
2021-11-23 20:21:47 +0100

    task-04.2

--- code/task-04.2/sketch.js
+++ code/task-04.2/sketch.js
@@ -1,7 +1,20 @@
-function setup() {
-  createCanvas(400, 400);
-}
+const sketchHeight = 400;
+const sketchWeight = 400;
 
-function draw() {
-  background(0);
+
+function setup() {
+
+  createCanvas(sketchWeight, sketchHeight);
+  background(255);
+
+
+  fill(10,20,30,40);
+
+  translate(200, 200);
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 60, 80, 120);
+    rotate(PI/5);
+  }
+  save("sketch.png");
 }
